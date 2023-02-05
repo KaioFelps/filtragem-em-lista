10292cd5ea6f30152404852eea5f5f81ed355793
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-02-05 11:05:00 -0300

    :rocket: initial commit

---
+++ lib/filtragem.ex
@@ -0,0 +1,18 @@
+defmodule Filtragem do
+  @moduledoc """
+  Documentation for `Filtragem`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Filtragem.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/filtragem_test.exs
@@ -0,0 +1,8 @@
+defmodule FiltragemTest do
+  use ExUnit.Case
+  doctest Filtragem
+
+  test "greets the world" do
+    assert Filtragem.hello() == :world
+  end
+end
